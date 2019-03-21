9a57226e1bb3ecfdb6629ad0c7a5feefdbd0342d
fEng1sHui <umanyura@mail.ru>
2019-03-21 22:37:13 +0100

    Add product controller, outputting product data from database

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -5,104 +5,23 @@
 @endsection
 
 @section('content')
-	<div class="row">
-		<div class="col-sm-6 col-md-4">
-			<div class="img-thumbnail">
-				<img src="https://www.billleonebookseller.com/pictures/7594.jpg?v=1429646836" alt="..." class="rounded mx-auto d-block img-responsive">
-				<div class="caption">
-					<h3>Product Title</h3>
-					<p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure.</p>
-					<div class="clearfix">
-						<div class="float-left price">$12</div>
-						<a href="#" class="btn btn-success float-right" role="button">Add to cart</a>
+	@foreach($products->chunk(3) as $productChunk)
+		<div class="row">
+			@foreach($productChunk as $product)
+				<div class="col-sm-6 col-md-4">
+					<div class="img-thumbnail">
+						<img src="{{ $product->imagePath }}" alt="..." class="rounded mx-auto d-block img-responsive">
+						<div class="caption">
+							<h3>{{ $product->title }}</h3>
+							<p class="description">{{ $product->description }}</p>
+							<div class="clearfix">
+								<div class="float-left price">$12</div>
+								<a href="#" class="btn btn-success float-right" role="button">Add to cart</a>
+							</div>
+						</div>
 					</div>
 				</div>
-			</div>
+			@endforeach
 		</div>
-		<div class="col-sm-6 col-md-4">
-			<div class="img-thumbnail">
-				<img src="https://www.billleonebookseller.com/pictures/7594.jpg?v=1429646836" alt="..." class="rounded mx-auto d-block img-responsive">
-				<div class="caption">
-					<h3>Product Title</h3>
-					<p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure.</p>
-					<div class="clearfix">
-						<div class="float-left price">$12</div>
-						<a href="#" class="btn btn-success float-right" role="button">Add to cart</a>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="col-sm-6 col-md-4">
-			<div class="img-thumbnail">
-				<img src="https://www.billleonebookseller.com/pictures/7594.jpg?v=1429646836" alt="..." class="rounded mx-auto d-block img-responsive">
-				<div class="caption">
-					<h3>Product Title</h3>
-					<p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure.</p>
-					<div class="clearfix">
-						<div class="float-left price">$12</div>
-						<a href="#" class="btn btn-success float-right" role="button">Add to cart</a>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-sm-6 col-md-4">
-			<div class="img-thumbnail">
-				<img src="https://www.billleonebookseller.com/pictures/7594.jpg?v=1429646836" alt="..." class="rounded mx-auto d-block img-responsive">
-				<div class="caption">
-					<h3>Product Title</h3>
-					<p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure.</p>
-					<div class="clearfix">
-						<div class="float-left price">$12</div>
-						<a href="#" class="btn btn-success float-right" role="button">Add to cart</a>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="col-sm-6 col-md-4">
-			<div class="img-thumbnail">
-				<img src="https://www.billleonebookseller.com/pictures/7594.jpg?v=1429646836" alt="..." class="rounded mx-auto d-block img-responsive">
-				<div class="caption">
-					<h3>Product Title</h3>
-					<p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure.</p>
-					<div class="clearfix">
-						<div class="float-left price">$12</div>
-						<a href="#" class="btn btn-success float-right" role="button">Add to cart</a>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="col-sm-6 col-md-4">
-			<div class="img-thumbnail">
-				<img src="https://www.billleonebookseller.com/pictures/7594.jpg?v=1429646836" alt="..." class="rounded mx-auto d-block img-responsive">
-				<div class="caption">
-					<h3>Product Title</h3>
-					<p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure.</p>
-					<div class="clearfix">
-						<div class="float-left price">$12</div>
-						<a href="#" class="btn btn-success float-right" role="button">Add to cart</a>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+	@endforeach
 @endsection
